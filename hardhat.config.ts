aa998ed2add6798bde66abefc668db69ff678958
Syed Hasnain <hasnain.shahzeb@gmail.com>
2024-07-17 10:10:21 -0600

    fix: optimizer updates + readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
